98c4a9615987e845357a44868cab4bfbcd1568eb
MAGHC <windowsapple95@gmail.com>
2024-03-24 16:34:42 +0900

    feat: PageItem component

--- src/components/page/Page.ts
+++ src/components/page/Page.ts
@@ -5,3 +5,21 @@ export class PageComponent extends Component<HTMLUListElement> {
     super('<ul class="page">페이지 컴포넌트</ul>');
   }
 }
+
+export class PageItem extends Component<HTMLElement> {
+  constructor() {
+    super(`<li class="page-item">
+    <section class="page-item_body"></section>
+    <div class="page-itme_controls">
+      <button class="close">X</button>
+    </div>
+  </li>
+  `);
+  }
+
+  addChild(child: Component<HTMLElement>) {
+    const pageItemBody = this.el.querySelector('.page-itme_body')! as HTMLElement;
+
+    child.attachTo(pageItemBody);
+  }
+}
